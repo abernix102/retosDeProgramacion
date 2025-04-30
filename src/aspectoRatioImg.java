41a7b80ce4999c2b290a7079fc1ac0ed64ae7e8e
Nahuel <nahuelo841@gmail.com>
2025-04-30 11:23:44 -0300

    tarea

--- src/aspectoRatioImg.java
+++ src/aspectoRatioImg.java
@@ -1,5 +1,12 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 public class aspectoRatioImg {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
          * Crea un programa que se encargue de calcular el aspect ratio de una
          * imagen a partir de una url.
@@ -8,5 +15,29 @@ public class aspectoRatioImg {
          * - Por ratio hacemos referencia por ejemplo a los "16:9" de una
          *   imagen de 1920*1080px.
          */
+
+        String imageUrl = "";
+        URL url = new URL("https://i.pinimg.com/736x/c2/24/a8/c224a86cf9a708ec6f5c3b49e557dcaa.jpg");
+        BufferedImage  image = ImageIO.read(url);
+        String[] relaciones = {"16:9", "4:3", "1:1", "3:2", "21:9"};
+        double[] valores = {16.0/9.0, 4.0/3.0, 1.0, 3.0/2.0, 21.0/9.0};
+
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+        double aspectRatio = (double) width / height;
+        System.out.println("Ancho: " + width + ", Alto: " + height);
+        System.out.println("el ratio de la imagen es: " + aspectRatio);
+        String aspectoAproximado = "";
+        double diferenciaMinima = Double.MAX_VALUE;
+        //la constante más grande que puede tener un número tipo double en Java.
+        for (int i = 0; i < valores.length; i++) {
+            double diferencia = Math.abs(aspectRatio - valores[i]);
+            if(diferencia < diferenciaMinima){
+                diferenciaMinima = diferencia;
+                aspectoAproximado = relaciones[i];
+            }
+        }
+        System.out.println(aspectoAproximado);
     }
 }
--- src/invirtiendoCadenas.java
+++ src/invirtiendoCadenas.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class invirtiendoCadenas {
     public static void main(String[] args) {
         /*
@@ -5,5 +8,12 @@ public class invirtiendoCadenas {
          * sin usar funciones propias del lenguaje que lo hagan de forma automática.
          * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
          */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese la palabra: ");
+        String valorDeEntrada = scanner.nextLine();
+        char [] caracteres = valorDeEntrada.toCharArray();
+        for (int i = caracteres.length - 1; i >= 0; i--) {
+            System.out.print(caracteres[i]);
+        }
     }
 }
