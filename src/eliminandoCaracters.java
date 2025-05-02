50e8afcc73e9dd97506fb623fac54db9112408d0
Nahuel <nahuelo841@gmail.com>
2025-05-02 12:53:51 -0300

    tarea de hoy

--- src/eliminandoCaracters.java
+++ src/eliminandoCaracters.java
@@ -1,6 +1,27 @@
 public class eliminandoCaracters {
     public static void main(String[] args) {
-        /*
+        System.out.println(eliminarCaracter("Me gQQQQQQusta Java","Me gusta Kotlin"));
+    }
+    public  static  String eliminarCaracter(String str1, String str2){
+        str1 = str1.toLowerCase().replaceAll("\\s+", "");
+        str2= str2.toLowerCase().replaceAll("\\s+", "");
+        StringBuilder out1 = new StringBuilder();
+        StringBuilder out2 = new StringBuilder();
+        for(char c : str1.toCharArray()){
+            if(!str2.contains(String.valueOf(c)) && out1.indexOf(String.valueOf(c)) == -1){
+                out1.append(c);
+            }
+        }
+        for(char c : str2.toCharArray()){
+            if(!str1.contains(String.valueOf(c))&& out2.indexOf(String.valueOf(c)) == -1){
+                out2.append(c);
+            }
+        }
+        return "caracteres presentes en la str1 pero NO estén presentes en str2. son :" + out1  + "\n" + "caracteres presentes en la str2 pero NO estén presentes en str1 son : " + out2;
+    }
+}
+
+   /*
         "\\s+", ""
          * Crea una función que reciba dos cadenas como parámetro (str1, str2)
          * e imprima otras dos cadenas como salida (out1, out2).
@@ -9,16 +30,3 @@ public class eliminandoCaracters {
          * - out2 contendrá todos los caracteres presentes en la str2 pero NO
          *   estén presentes en str1.
          */
-        System.out.println(eliminarCaracter("Me gusta Java","Me gusta Kotlin"));
-    }
-    public  static  String eliminarCaracter(String str1, String str2){
-        StringBuilder out1 = new StringBuilder();
-        StringBuilder out2 = new StringBuilder();
-        for(char c : str1.toLowerCase().replaceAll("\\s+", "")){
-            if(!str2.toLowerCase().contains(c)){
-                out1.append(c);
-            }
-        }
-        return  "zapallo";
-    }
-}
